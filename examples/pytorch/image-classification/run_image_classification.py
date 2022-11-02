cc879209b9502481f3fa6d40c4e92f7bd097192d
mengfeil <mengfei.li@intel.com>
2022-11-02 02:44:28 -0700

    Revert "add image-classfication"
    This reverts commit d6690aac6f853e52c6087402e5ceb6944b78a515.

--- examples/pytorch/image-classification/run_image_classification.py
+++ examples/pytorch/image-classification/run_image_classification.py
@@ -249,7 +249,7 @@ def main():
 
     # Prepare label mappings.
     # We'll include these in the model's config to get human readable labels in the Inference API.
-    labels = dataset["validation"].features["labels"].names
+    labels = dataset["train"].features["labels"].names
     label2id, id2label = dict(), dict()
     for i, label in enumerate(labels):
         label2id[label] = str(i)
