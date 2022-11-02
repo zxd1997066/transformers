66ceae95b5344f81f26fd1028b063e5c12e28110
mengfeil <mengfei.li@intel.com>
2022-11-02 01:19:37 -0700

    add amp autocast note

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -2832,6 +2832,7 @@ class Trainer:
                 on_trace_ready=self.trace_handler,
             ) as prof:
                 if self.args.precision == "bfloat16":
+                    print("---- Use cpu AMP bfloat16")
                     with torch.cpu.amp.autocast(enabled=True, dtype=torch.bfloat16):
                         output = eval_loop(
                             eval_dataloader,
@@ -2844,6 +2845,7 @@ class Trainer:
                             prof=prof,
                         )
                 elif self.args.precision == "float16":
+                    print("---- Use cuda AMP float16")
                     with torch.cuda.amp.autocast(enabled=True, dtype=torch.float16):
                         output = eval_loop(
                             eval_dataloader,
@@ -2868,6 +2870,7 @@ class Trainer:
                     )
         else:
             if self.args.precision == "bfloat16":
+                print("---- Use cpu AMP bfloat16")
                 with torch.cpu.amp.autocast(enabled=True, dtype=torch.bfloat16):
                     output = eval_loop(
                         eval_dataloader,
@@ -2879,6 +2882,7 @@ class Trainer:
                         metric_key_prefix=metric_key_prefix,
                     )
             elif self.args.precision == "float16":
+                print("---- Use cuda AMP float16")
                 with torch.cuda.amp.autocast(enabled=True, dtype=torch.float16):
                     output = eval_loop(
                         eval_dataloader,
