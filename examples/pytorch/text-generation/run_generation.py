73f4dcb75da117d9a9ad0c31df7a0f715d48e594
xiangdong <40376367+zxd1997066@users.noreply.github.com>
2024-03-18 16:31:09 +0800

    Update run_generation.py

--- examples/pytorch/text-generation/run_generation.py
+++ examples/pytorch/text-generation/run_generation.py
@@ -292,8 +292,8 @@ def main():
         total_sample = 0
         total_time = 0.0
         if args.compile:
-            model.generate = torch.compile(model.generate, backend=args.backend, options={"freezing": True})
-            # model.generate = torch.compile(model.generate, backend=args.backend, dynamic=True)
+            # model.generate = torch.compile(model.generate, backend=args.backend, options={"freezing": True})
+            model.generate = torch.compile(model.generate, backend=args.backend, options={"freezing": True}, dynamic=True)
         if args.precision == "bfloat16":
             print("---- Use bfloat16 AMP")
             with torch.cpu.amp.autocast(enabled=True, dtype=torch.bfloat16):
