734b63fc4c7530142b23ac956b7d31de81042e0b
xiangdong <40376367+zxd1997066@users.noreply.github.com>
2024-04-15 01:34:24 +0800

    Update trainer.py

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -2825,8 +2825,8 @@ class Trainer:
 
         eval_loop = self.prediction_loop if self.args.use_legacy_prediction_loop else self.evaluation_loop
         if self.args.profile:
-            torch._inductor.config.profiler_mark_wrapper_call = True
-            torch._inductor.config.cpp.enable_kernel_profile = True
+            # torch._inductor.config.profiler_mark_wrapper_call = True
+            # torch._inductor.config.cpp.enable_kernel_profile = True
             prof_act = [torch.profiler.ProfilerActivity.CUDA, torch.profiler.ProfilerActivity.CPU]
             with torch.profiler.profile(
                 activities=prof_act,
