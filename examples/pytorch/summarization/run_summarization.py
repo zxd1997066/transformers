863a94bb46baebce27fa5e4f6fc4cc37fee59f84
xiangdong <40376367+zxd1997066@users.noreply.github.com>
2024-08-04 19:59:52 +0800

    Update run_summarization.py

--- examples/pytorch/summarization/run_summarization.py
+++ examples/pytorch/summarization/run_summarization.py
@@ -373,6 +373,7 @@ def main():
             data_args.dataset_name,
             data_args.dataset_config_name,
             cache_dir=model_args.cache_dir,
+            trust_remote_code=True,
             use_auth_token=True if model_args.use_auth_token else None,
         )
     else:
@@ -390,6 +391,7 @@ def main():
             extension,
             data_files=data_files,
             cache_dir=model_args.cache_dir,
+            trust_remote_code=True,
             use_auth_token=True if model_args.use_auth_token else None,
         )
     # See more about loading any type of standard or custom dataset (from files, python dict, pandas DataFrame, etc) at
